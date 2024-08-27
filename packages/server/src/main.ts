ba418f3db1c25e4df08487923bddd98139a99fa8
soumajit <soumojitghosh02@gmail.com>
2024-08-27 09:25:14 +0530

    refresh token added back

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interseptor';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
