e36248b646d6f5ad0d12c7776a87285ea0f2683b
soumajit <soumojitghosh02@gmail.com>
2024-09-05 10:05:37 +0530

    minor UI changes

--- packages/client/src/components/common/root-navbar/root-navbar.component.tsx
+++ packages/client/src/components/common/root-navbar/root-navbar.component.tsx
@@ -1,23 +1,24 @@
-import fullLogo from "../../../assets/logo/airbnb-full-logo.svg"
-import RootNavbarSearch from "./root-navbar-search.component.tsx";
+import fullLogo from '../../../assets/logo/airbnb-full-logo.svg';
+import RootNavbarSearch from './root-navbar-search.component.tsx';
+import HamburgerUserAvatarBtn from './hamburger-user-avatar-btn.component.tsx';
 
 export default function RootNavbar() {
   return (
     <>
       <nav className="navbar container mx-auto my-2">
         <div className="navbar-start">
-          <img src={fullLogo} alt={"airbnb-logo"} className={"w-28"}/>
+          <img src={fullLogo} alt={'airbnb-logo'} className={'w-28'} />
         </div>
 
         <div className="navbar-center">
-          <RootNavbarSearch/>
+          <RootNavbarSearch />
         </div>
 
         <div className="navbar-end">
-
+          <HamburgerUserAvatarBtn />
         </div>
       </nav>
-      <div className="divider my-0"/>
+      <div className="divider my-0" />
     </>
-  )
+  );
 }
