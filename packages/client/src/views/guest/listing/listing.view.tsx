951124544972eac874b8dc09d27cbf058ffabb27
soumajit <soumojitghosh02@gmail.com>
2024-10-26 09:40:16 +0530

    booking card completed in view listing page

--- packages/client/src/views/guest/listing/listing.view.tsx
+++ packages/client/src/views/guest/listing/listing.view.tsx
@@ -3,17 +3,23 @@ import { useFetchListingByID } from '../../../hooks/api-hooks/hosting/listing/fe
 import { Listing } from '../../../types/listing/listing.type.ts';
 import ListingBentoGrid from './components/listing-images-grid.component.tsx';
 import ListingLocationDetails from './components/listing-location-details.component.tsx';
+import ListingBookingCard from './components/listing-booking-card.component.tsx';
 
 
 function ListingView() {
 
   const { listingId } = useParams();
+  // const [searchParams] = useSearchParams();
+
   const { data, isFetched } = useFetchListingByID(listingId as string);
+  //
+  // const checkInDate = searchParams.get('checkIn') ? new Date(String(searchParams.get('checkIn'))) : new Date();
+  // const checkOutDate = searchParams.get('checkOut') ? new Date(String(searchParams.get('checkOut'))) : null;
 
   const fetchedListing = data?.data as Listing;
 
   return isFetched && (
-    <div className={'grid grid-cols-5 gap-8 mt-4'}>
+    <div className={'grid grid-cols-5 gap-8 my-4'}>
       <div className={'col-span-5 flex justify-between'}>
         <h2 className={'font-semibold text-3xl'}>{fetchedListing.title}</h2>
       </div>
@@ -36,6 +42,10 @@ function ListingView() {
         </div>
 
 
+      </div>
+
+      <div className="col-span-2">
+        <ListingBookingCard listing={fetchedListing} />
       </div>
 
     </div>
