c14c14804b9c20ddad038c7e4058334f21c7a0d7
Soumajit Ghosh <soumojitghosh02@gmail.com>
2024-12-04 09:31:26 +0530

    some bug fixes and style improvements

--- packages/client/src/views/guest/dashboard.view.tsx
+++ packages/client/src/views/guest/dashboard.view.tsx
@@ -5,7 +5,7 @@ export default function DashboardView() {
   return (
     <EmptyCard className={'h-96 text -z-10'}>
       <p>Welcome to Airbnb Clone! Search something to get started.</p>
-      <p>or <Link className={'btn btn-outline btn-active'} to={'/hosting/become-a-host'}>Create Your Own</Link></p>
+      <p>or <Link className={'btn btn-outline ml-5'} to={'/hosting/become-a-host'}>Create your listing</Link></p>
     </EmptyCard>
   );
 }
